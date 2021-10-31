e028adffaa0b9fa9bfa21a681ad404a15b45637a
burakbf55 <burak0009000@gmail.com>
2021-10-31 15:14:54 +0300

    Herokuapp ile Live Yapmak İçin Kodları Düzenledik

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hc86_o7hz$dut2#(luyw3g&%2lh^=i#awm3+g71q!xq5l)iduq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exam-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
